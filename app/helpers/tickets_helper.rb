76bafcf81daee8ce99066ba89faf09b0bf924bdd
Elliott Hebert <elliott.hebert.pro@gmail.com>
2017-09-26 23:59:43 +0200

    create priority method

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -1,7 +1,7 @@
 module TicketsHelper
   STATUS = ["New", "Assigned", "In Progress", "Resolved",]
   SERVICES = ["Internet", "Mail", "Work Station", "Phones", "Printer", "Other"]
-  IMPACTS = %w(Critical Sigificant Moderate Minor)
-  URGENCIES = %w(Urgent Hight Medium Low)
+  IMPACTS = { "Critical" => 1, "Sigificant" => 2, "Moderate" => 3, "Minor" => 4 }
+  URGENCIES = {"Urgent" => 1, "High" => 2, "Medium" => 3, "Low" => 4}
   SUPPORT_LEVELS = [1,2,3]
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,2 +1,8 @@
 class Ticket < ApplicationRecord
+  def priority
+    impact_number = TicketsHelper::IMPACTS[impact]
+    urgency_number = TicketsHelper::URGENCIES[urgency]
+
+    return (impact_number + urgency_number) - 1
+  end
 end
